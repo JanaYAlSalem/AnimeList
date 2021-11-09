06358139b3bc1e800ab2a2437e5ee7b1131dbffb
Jana <Jana.Yousef.ruh.k@tuwaiq.edu.sa>
2021-11-09 20:19:27 +0300

    Initial commit

---
+++ app/src/main/java/com/example/animelist/data/Dataset.kt
@@ -0,0 +1,12 @@
+package com.example.animelist.data
+import com.example.animelist.model.anime
+
+object Dataset {
+    // object to return data
+val item : List<anime> = listOf(
+    //val imge: Int ,val name : String ,val numOfEp : Int ,val Season: Int)
+    anime(1,R.string,20,2),
+
+
+        )
+}
---
+++ app/src/main/java/com/example/animelist/model/anime.kt
@@ -0,0 +1,6 @@
+package com.example.animelist.model
+
+data class anime (val imge: Int ,
+                  val name : String ,
+                  val numOfEp : Int ,
+                  val Season: Int)
