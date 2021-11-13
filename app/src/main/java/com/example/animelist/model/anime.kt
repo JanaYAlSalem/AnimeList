ce8b7f41efbd330abbf3fafee7a9326603584189
RMSAJ <92260209+RMSAJ@users.noreply.github.com>
2021-11-13 20:43:22 +0300

    ADDED THE NEW PACKAGE

--- app/src/main/java/com/example/animelist/model/anime.kt
+++ app/src/main/java/com/example/animelist/model/anime.kt
@@ -1,7 +1,31 @@
 package com.example.animelist.model
 
-data class anime (val imge: Int ,
-                  val name : Int ,
-                  val numOfEp : Int ,
-                  val Season: Int,
-val type:Int)
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+class animeViewModel ()
+      {
+    private val _imge = MutableLiveData<Int>()
+     val image : LiveData<Int> = _imge
+    private val _name = MutableLiveData<Int>()
+     val name : LiveData<Int> = _name
+    private val _numOfEp = MutableLiveData<Int>()
+     val numberOfEpisode : LiveData<Int> = _numOfEp
+    private val _season = MutableLiveData<Int>()
+    val season : LiveData<Int> = _season
+    private val _type = MutableLiveData<Int>()
+    val type : LiveData<Int> = _type
+
+    fun setAnimeName (name: Int) {
+        _name.value = name
+     }
+     fun setAnimeImage (imge: Int) {
+        _imge.value = imge
+     }
+     fun setNumberofEpisodes (numberofEpisode: Int) {
+        _numOfEp.value = numberofEpisode
+     }
+     fun setSeasone (seasonNum: Int) {
+        _season.value = seasonNum
+     }
+ }
